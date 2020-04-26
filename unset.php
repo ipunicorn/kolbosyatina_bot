d5835081028b6160b37e82bbde0fb751a7f4b1b5
upunicorn <irina.somnias@gmail.com>
2020-04-26 22:09:44 +0300

    set & unset commands load env

--- unset.php
+++ unset.php
@@ -2,6 +2,9 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+$env = Dotenv\Dotenv::createImmutable(__DIR__);
+$env->load();
+
 $botApiKey = getenv("BOT_API_KEY");
 $botUsername = getenv("BOT_USERNAME");
 
